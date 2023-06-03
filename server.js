e02501549d8dfa3af206c2b52ae10d3e897864c6
shaikhshehzad <shehzadali3113@gmail.com>
2023-06-03 18:02:40 +0530

    base route

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const dotenv = require("dotenv").config();
 
 const app = express();
 const port = process.env.port || 5000;
+const siteOwner = process.env.siteOwner || "Shehzad "  ;
+
+app.get("", (req, res) => {
+    res.send(`Contact Manager ${siteOwner}`  );
+  });
+
 
 app.get("/api/v1/contacts", (req, res) => {
   res.send("Get all Contacts");
